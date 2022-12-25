eb18a25115f9cf4fd3ff797da75d7a2c6ed2cf04
Amina <ba.amina.01@gmail.com>
2022-12-24 19:39:37 -0500

    Create main component app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { HttpResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { map, Observable, of, tap } from 'rxjs';
+import { JobDTO } from './dto/job.dto';
+import { SearchJobService } from './search.job.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'JobSearch';
+  jobs$: Observable<JobDTO[]> = of();
+
+  constructor (private searchJobService : SearchJobService) {
+
+  }
+
+  ngOnInit() {
+    this.getJobs();
+  }
+
+  getJobs() {
+    //console.log("test coomp")
+    this.jobs$ = this.searchJobService.getJob().pipe(
+      tap(data => {console.log(data)})
+      );
+  }
+
 }
